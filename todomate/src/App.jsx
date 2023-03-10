df782ab6b92520257033b97df4680b39bacc8995
danii <danii0110@naver.com>
2023-03-10 16:42:46 +0900

    todolist 초안
    초기 세팅 및 코알누 참고

---
+++ todomate/src/App.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './App.css';
+import TodoBoard from './components/TodoBoard';
+
+//1. 인풋창이 있고 버튼이 있다.
+//2. 인풋창에 값을 입력하고 버튼을 클릭하면 아이템이 추가가 된다.
+//3. 아이템 삭제 버튼을 누르면 삭제가 가능하다.
+function App() {
+    const [inputValue, setInputValue] = useState('')
+    const [todoList, setTodoList]=useState([])
+    const addItem=() => {
+        console.log("im herererere!", inputValue)
+        setTodoList([...todoList,inputValue])
+    }
+    return (
+        <main>
+            <input value={inputValue}type="text"onChange={(event)=>setInputValue(event.target.value)}/>
+            <button onClick={addItem}>추가</button>
+
+            <TodoBoard todoList={todoList}/>
+        </main>
+    );
+}
+export default App;
